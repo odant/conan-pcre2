0398f0a70187581aec4ba479ebe77c1cd589c167
Arkady Yudintsev <udincev@odant.ru>
2022-12-06 11:35:21 +0300

    Fixing the regex.h file

--- regex.h
+++ regex.h
@@ -1,7 +1,8 @@
-#ifndef _PCREPOSIX_REGEX_H
-#define _PCREPOSIX_REGEX_H
+#ifndef _PCRE2POSIX_REGEX_H
+#define _PCRE2POSIX_REGEX_H
 
-#include <pcre2.h>
-#include <pcre2posix.h>
+#define PCRE2_CODE_UNIT_WIDTH 8
+#include "pcre2.h"
+#include "pcre2posix.h"
 
-#endif // _PCREPOSIX_REGEX_H
+#endif // _PCRE2POSIX_REGEX_H
